0b8191f93d51aeff7465606fa348d663d056cd22
ChristianHesberg <cchesberg@gmail.com>
2024-11-26 11:28:26 +0100

    refactor

---
+++ backend/core/ports/IUserDataPort.cs
@@ -0,0 +1,8 @@
+﻿using core.models;
+
+namespace core.ports;
+
+public interface IUserDataPort
+{
+    public void AddUserKeyPair(string userId, byte[] publicKey,  AesGcmEncryptionOutput encryptedPrivateKey);
+}
---
+++ backend/core/services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using core.ports;
+
+namespace core.services;
+
+public class UserService(IUserDataPort userDataPort)
+{
+    private readonly CryptoUtils _utils = new();
+    private void GenerateRsaKeyPair(string password, string userId)
+    {
+        var passwordToBytes = Encoding.UTF8.GetBytes(password);
+        var keyPair = _utils.GenerateRsaKeyPair();
+        var encryptionOutput = _utils.Encrypt(keyPair.PrivateKey, passwordToBytes);
+        
+        userDataPort.AddUserKeyPair(userId, keyPair.PublicKey, encryptionOutput);
+    }
+}
