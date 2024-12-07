3476660d1bd7d770568fcc94839b944dd88bf7ab
victor <90904666+Victorkaffekande@users.noreply.github.com>
2024-12-07 16:06:16 +0100

    Single sign on (#8)
    * inital google SSO implementation
    
    * added tailwind
    
    * npm audit fix
    
    * initial styling

--- frontend/file_samurai_frontend/src/App.tsx
+++ frontend/file_samurai_frontend/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {GoogleLogin, googleLogout, GoogleOAuthProvider} from '@react-oauth/google';
+import {useAuth} from "./providers/AuthProvider";
+import {Login} from "./pages/Login";
+import {Route, Routes} from "react-router";
+import {Home} from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={"bg-neutral-950 text-gray-300 flex h-screen"}>
+            <div className={"container mx-auto"}>
+                <Routes>
+                    <Route path={"/home"} element={<Home/>}/>
+                    <Route path={"/"} element={<Login/>}/>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/file_samurai_frontend/src/pages/Login.tsx
@@ -0,0 +1,35 @@
+import {CredentialResponse, GoogleLogin} from "@react-oauth/google";
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import {useAuth} from "../providers/AuthProvider";
+
+export function Login() {
+    const navigate = useNavigate()
+    const {user, login, logout} = useAuth();
+
+    const handleSuccess = (credentialResponse: CredentialResponse) => {
+        login(credentialResponse)
+        navigate("/home")
+        console.log(credentialResponse);
+    }
+
+    return (
+        <div className={"flex flex-col  items-center h-screen mt-40"}>
+            <p className={"text-2xl"}>Welcome to FileSamurai</p>
+            <div className={"flex-col"}>
+                <p className={"text-2xl text-center p-2"}> Login</p>
+                <GoogleLogin
+                    onSuccess={credentialResponse => {
+                        handleSuccess(credentialResponse)
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                />
+            </div>
+
+        </div>
+
+    )
+
+}
