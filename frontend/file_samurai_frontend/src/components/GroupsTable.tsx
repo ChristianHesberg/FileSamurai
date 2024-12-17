740890c603ecfcc670908c92fc090daf2c0cc530
Victorkaffekande <victor.gugerel@gmail.com>
2024-12-17 14:54:43 +0100

    scaffolding

--- frontend/file_samurai_frontend/src/components/GroupsTable.tsx
+++ frontend/file_samurai_frontend/src/components/GroupsTable.tsx
@@ -1,11 +1,58 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBars} from "@fortawesome/free-solid-svg-icons/faBars";
 import getGroups from "../templateData/GroupData"
-import GroupOptionsDropdown from "./GroupOptionsDropdown";
+import TableOptionsBtn from "./TableOptionsBtn";
+import Modal from "./Modal";
+import React, {useState} from "react";
 
 export function GroupsTable() {
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => setModalOpen(true);
+    const closeModal = () => {
+        setModalOpen(false)
+    };
+    const handleAddMemberClick = () => {
+
+    }
+    const handleDeleteGroupClick = () => {
+
+    }
+
+    const addMemberBtn = () => {
+        return (
+            <div>
+                <button
+                    className="block px-4 py-2 text-sm bg-lime-900 hover:bg-lime-700 w-full rounded"
+                    role="menuitem"
+                    onClick={openModal}
+                >
+                    Add member
+                </button>
+                <Modal isOpen={isModalOpen} onClose={closeModal}/>
+            </div>
+        )
+
+
+    }
+
+    const deleteGroupBtn = () => {
+        return (
+            <button
+                className="block px-4 py-2 text-sm bg-red-900 hover:bg-red-800 w-full  rounded"
+                role="menuitem"
+                onClick={handleDeleteGroupClick}
+            >
+                Delete Group
+            </button>
+        )
+    }
+    const buttons = () => {
+        return [addMemberBtn(), deleteGroupBtn()]
+    }
     const allGroups = getGroups()
     return (
+
         <div>
             <table className={"min-w-full text-left text-sm font-light text-surface dark:text-white"}>
                 <thead className={"border-b border-neutral-200 font-medium dark:border-white/10"}>
@@ -17,6 +64,7 @@ export function GroupsTable() {
                         Member count
                     </th>
                     <th scope={"col"} className={"px-6 py-4"}>
+
                     </th>
                 </tr>
                 </thead>
@@ -30,7 +78,7 @@ export function GroupsTable() {
                             {group.members.length} members
                         </td>
                         <td>
-                            <GroupOptionsDropdown/>
+                            <TableOptionsBtn children={buttons()}/>
                         </td>
                     </tr>
                 ))}
--- frontend/file_samurai_frontend/src/pages/Files.tsx
+++ frontend/file_samurai_frontend/src/pages/Files.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import FileTable from "../components/FileTable";
+import UploadFileBtn from "../components/UploadFileBtn";
 
 export function Files() {
     return (
-
                 <div className={"flex-col"}>
-                    <h1 className={"text-lg"}>All files</h1>
+                    <div className={"flex justify-between"}>
+                        <h1 className={"text-lg"}>All files</h1>
+                        <UploadFileBtn/>
+                    </div>
                     <FileTable/>
                 </div>
-
-
     )
 
 }
