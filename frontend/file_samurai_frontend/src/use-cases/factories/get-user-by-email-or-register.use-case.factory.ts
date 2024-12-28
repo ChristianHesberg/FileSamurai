e8bc8a125ca39e103c1b9295fb1f087520ab43dd
ChristianHesberg <cchesberg@gmail.com>
2024-12-28 16:05:52 +0100

    make factory

---
+++ frontend/file_samurai_frontend/src/use-cases/factories/get-user-by-email-or-register.use-case.factory.ts
@@ -0,0 +1,10 @@
+import {GetUserByEmailOrRegisterUseCase} from "../get-user-by-email-or-register.use-case";
+import {UserService} from "../../services/user.service";
+
+export class GetUserByEmailOrRegisterUseCaseFactory {
+    static create(){
+        return new GetUserByEmailOrRegisterUseCase(
+            new UserService()
+        );
+    }
+}
