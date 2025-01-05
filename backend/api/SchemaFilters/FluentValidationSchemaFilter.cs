1474880fed45289d97f1e6b486a8484ec481d1c4
Victorkaffekande <victor.gugerel@gmail.com>
2025-01-05 14:18:43 +0100

    inital, does not work

---
+++ backend/api/SchemaFilters/FluentValidationSchemaFilter.cs
@@ -0,0 +1,41 @@
+﻿using application.validation;
+using FluentValidation;
+using FluentValidation.Validators;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace api.SchemaFilters;
+
+public class FluentValidationSchemaFilter(IServiceProvider serviceProvider) : ISchemaFilter
+{
+    private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        var scope = _serviceProvider.CreateScope();
+        var validatorType = typeof(IValidator<>).MakeGenericType(context.Type);
+        var validator = scope.ServiceProvider.GetService(validatorType) as IValidator;
+        if (validator == null) return;
+        var rules = validator.CreateDescriptor().GetMembersWithValidators();
+
+        Console.WriteLine(rules);
+        
+        foreach (var group in rules)
+        {
+            var name = group.Key;
+            var rule = group;
+            if (schema.Properties.TryGetValue(name, out var property))
+
+            //var a =schema.Properties.Keys;
+            property.Description = "this is a filler desc";
+            foreach (var (validatorInstance, _) in group)
+            {
+                if (validatorInstance is GetFileOrAccessInputDtoValidator)
+                {
+                    property.Format = "uuid"; // Set OpenAPI format to indicate it's a GUID
+                    property.Description = "Must be a valid GUID.";
+                }
+            }
+        }
+    }
+}
