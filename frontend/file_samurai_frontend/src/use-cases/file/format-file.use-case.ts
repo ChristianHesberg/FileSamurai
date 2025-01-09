ea30800d1d19d6feb80fab3fdf6cb2a1a28ecc81
ChristianHesberg <cchesberg@gmail.com>
2025-01-09 11:44:41 +0100

    add library file-type

---
+++ frontend/file_samurai_frontend/src/use-cases/file/format-file.use-case.ts
@@ -0,0 +1,34 @@
+import {fileTypeFromBuffer} from "file-type";
+
+export class FormatFileUseCase{
+
+    async execute(file: Buffer): Promise<void> {
+        try {
+
+            const fileType = await fileTypeFromBuffer(file);
+            if(fileType == null) throw new Error("Invalid file type");
+            // Create a Blob from the decrypted content
+            const blob = new Blob([file], { type: fileType.mime });
+
+            // Create a URL for the Blob
+            const url = URL.createObjectURL(blob);
+
+            // Create a link element
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'your download link';
+
+            // Append the link to the document body and trigger a click event
+            document.body.appendChild(a);
+            a.click();
+
+            // Clean up by revoking the Object URL and removing the link element
+            URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+
+            console.log('File downloaded successfully.');
+        } catch (error) {
+            console.error('Error decrypting or downloading the file:', error);
+        }
+    }
+}
