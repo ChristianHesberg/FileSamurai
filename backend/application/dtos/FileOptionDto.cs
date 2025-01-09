ff7cb4db0c5268defc3644d20fd959ae463e122f
ChristianHesberg <cchesberg@gmail.com>
2025-01-09 13:26:08 +0100

    sanitize all user facing input

--- backend/application/dtos/FileOptionDto.cs
+++ backend/application/dtos/FileOptionDto.cs
@@ -1,10 +1,17 @@
-﻿using core.models;
+﻿using application.transformers;
+using core.models;
 
 namespace application.dtos;
 
 public class FileOptionDto
 {
     public string Id { get; set; }
-    public string Name { get; set; }
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = InputSanitizer.Sanitize(value);
+    }
     public Roles Role { get; set; }
 }
--- backend/application/dtos/GroupDto.cs
+++ backend/application/dtos/GroupDto.cs
@@ -1,9 +1,16 @@
-﻿namespace application.dtos;
+﻿using application.transformers;
+
+namespace application.dtos;
 
 public class GroupDto
 {
     public string Id { get; set; }
-    public string Name { get; set; }
+    private string _name;  
+    public string Name  
+    {  
+        get => _name;  
+        set => _name = InputSanitizer.Sanitize(value);  
+    } 
     
     public string GroupEmail { get; set; }
 }
