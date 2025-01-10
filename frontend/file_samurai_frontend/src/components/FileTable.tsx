45c337f171eb02b4a2870f7a6d1d3932eec84f44
Victorkaffekande <victor.gugerel@gmail.com>
2025-01-10 11:51:00 +0100

    removed edit modal

--- frontend/file_samurai_frontend/src/components/FileTable.tsx
+++ frontend/file_samurai_frontend/src/components/FileTable.tsx
@@ -4,7 +4,6 @@ import {faCloudArrowDown} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {FileOption} from "../models/FileOption";
 import Modal from "./Modal";
-import {EditFileModal} from "./modals/EditFileModal";
 import {useUseCases} from "../providers/UseCaseProvider";
 import {useKey} from "../providers/KeyProvider";
 import {useAuth} from "../providers/AuthProvider";
@@ -19,28 +18,10 @@ interface FileTableProps {
 const FileTable: React.FC<FileTableProps> = ({files, setFiles}) => {
     const [selectedFile, setSelectedFile] = useState<FileOption | null>(null)
     const [openShareModal, setOpenShareModal] = useState<boolean>(false)
-    const [openEditModal, setOpenEditModal] = useState<boolean>(false)
     const {decryptFileUseCase, downloadFileUseCase, deleteFileUseCase} = useUseCases();
     const {retrieveKey} = useKey();
     const {user} = useAuth()
 
-
-    const editBtn = (fileOption: FileOption) => {
-        return (
-            <button
-                key={"memberBtn" + fileOption.id}
-                className="block px-4 py-2 text-sm bg-lime-900 hover:bg-lime-700 w-full rounded"
-                role="menuitem"
-                onClick={() => {
-                    setSelectedFile(fileOption)
-                    setOpenEditModal(true)
-                }}
-            >
-                Edit
-            </button>
-        )
-
-    }
 
     const shareBtn = (fileOption: FileOption) => {
         return (
@@ -104,7 +85,6 @@ const FileTable: React.FC<FileTableProps> = ({files, setFiles}) => {
             {downloadBtn(selectedFile)}
             {selectedFile.role == "Editor" ? (
                 <>
-                    {editBtn(selectedFile)}
                     {shareBtn(selectedFile)}
                     {deleteBtn(selectedFile)}
                 </>
@@ -118,8 +98,7 @@ const FileTable: React.FC<FileTableProps> = ({files, setFiles}) => {
             {selectedFile ? <Modal isOpen={openShareModal} onClose={() => clearSelectedFile(setOpenShareModal)}
                                    child={<ShareFileModal selectedFile={selectedFile}
                                                           onClose={() => clearSelectedFile(setOpenShareModal)}/>}/> : <></>}
-            {selectedFile ? <Modal isOpen={openEditModal} onClose={() => clearSelectedFile(setOpenEditModal)}
-                                   child={<EditFileModal selectedFile={selectedFile}/>}/> : <></>}
+
 
             <table className={"min-w-full text-left text-sm font-light text-surface dark:text-white"}>
                 <thead className={"border-b border-neutral-200 font-medium dark:border-white/10"}>
